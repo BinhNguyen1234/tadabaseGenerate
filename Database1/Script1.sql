afa07746259ea603d20c2e78a9ed15c29ddbbafb
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2023-09-01 18:12:21 +0700

    init

---
+++ Database1/Script1.sql
@@ -0,0 +1,18 @@
+﻿
+CREATE TABLE Students (
+    StudentID uniqueidentifier NOT NULL PRIMARY KEY,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255) NOT NULL,
+); 
+GO
+CREATE TABLE Enrollments (
+    EnrollmentID  uniqueidentifier NOT NULL PRIMARY KEY,
+    Grade varchar(255) NOT NULL,
+    StudentID uniqueidentifier FOREIGN KEY REFERENCES Students(StudentID),
+    CourseID uniqueidentifier FOREIGN KEY REFERENCES Courses(CourseID),
+);
+GO
+CREATE TABLE Courses (
+    CourseID  uniqueidentifier NOT NULL PRIMARY KEY,
+    Title varchar(255) NOT NULL,
+);
